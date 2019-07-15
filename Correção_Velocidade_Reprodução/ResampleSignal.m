29bb3fe953bef37b6e561ca2021d1e8a868b4ac2
Leonardo Barreto <52412843+leonardo-barreto@users.noreply.github.com>
2019-07-15 17:18:30 -0300

    updated resample
    Updated Resample script

--- Correção_Velocidade_Reprodução/ResampleSignal.m
+++ Correção_Velocidade_Reprodução/ResampleSignal.m
@@ -15,7 +15,7 @@ function y = ResampleSignal(x1,x2,x3)
     signalSize = length(inputSignal);
     filterCenter = 0;
     currentSampleIndex = 0;
-    newPeriod = 1/resampleFactor;
+    newPeriod = (1/resampleFactor);
 
     %Outputs
     outputSignal = zeros (1,signalSize);
@@ -27,23 +27,27 @@ function y = ResampleSignal(x1,x2,x3)
         else
             counter = 0;
             sample = 0;
-            rightStep = ceil(filterCenter) - filterCenter;
-            leftStep = filterCenter - floor(filterCenter);
-            while (counter < halfFilterCoeffs)
-                endSignal = 0;
-                if ((index = floor(filterCenter) - counter) > 0) %Calcula lado esquerdo da sinc
-                    sample = sample + inputSignal(index)*sinc();
+            gapRight = ceil(filterCenter) - filterCenter; %Distancias da sinc até a amostra mais próxima
+            gapLeft = filterCenter - floor(filterCenter);
+            while (counter < halfFilterCoeffs) %Calcula 2 termos por vez, do centro da sinc até as extremidades
+                leftConvolutionIndex = floor(filterCenter) - counter;
+                rightConvolutionIndex = ceil(filterCenter) + counter;
+                if ( > 0) %Calcula lado esquerdo da sinc
+                    sample = sample + inputSignal(leftConvolutionIndex)*sinc(gapLeft);
                 end
-
+                if ((convolutionIndex ) < signalSize) %Calcula lado direito da sinc
+                    sample = sample + inputSignal(rightConvolutionIndex)*sinc(gapRight);
+                end
+                counter = counter + 1; % percorre os pontos de encontro entre a sinc e o sinal.
+                gapLeft = gapLeft + 1;
+                gapRight = gapRight + 1;
             end
-
+            outputSignal(currentSampleIndex) = sample;
         end
-    
-
-        
+        currentSampleIndex = currentSampleIndex + 1;
+        filterCenter = filterCenter + newPeriod;
     end
     
-    
-    
+    y = outputSignal;
 
 end
