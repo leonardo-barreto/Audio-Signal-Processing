0b6e8a6b1f194caef313e8e9ada7c1b221301792
Leonardo Barreto <52412843+leonardo-barreto@users.noreply.github.com>
2019-07-19 17:05:13 -0300

    Updates bla bla

--- Correção_Velocidade_Reprodução/ResampleSignal.m
+++ Correção_Velocidade_Reprodução/ResampleSignal.m
@@ -4,49 +4,28 @@
 
 %Saidas: Sinal reamostrado: (outputSignal)
 
-function y = ResampleSignal(samplingFrequency,resampleFactor,halfFilterCoeffs,inputSignal)
+function outputSignal = ResampleSignal(resampleFactor,filterCoeffs,inputSignal)
     
     %Definitions
     signalSize = length(inputSignal);
-    filterCenter = 0;
+    newIndex = 0;
     currentSampleIndex = 1;
-    newFrequency = samplingFrequency*resampleFactor;
-    newFrequency
-    signalSize
-    newPeriod = (1/newFrequency);
+    newPeriod = (1/resampleFactor);
 
     %Outputs
-    outputSignal = zeros (1,signalSize);
-    
-    
+    outputSignal=zeros(1,signalSize);
+   
     %Code
-    while filterCenter <= signalSize
-        if (CheckInteger(filterCenter))
-            outputSignal(currentSampleIndex) = inputSignal(currentSampleIndex);
-        else
-            counter = 0;
+    while newIndex < signalSize
+            originalIndex = 1;
             sample = 0;
-            gapRight = ceil(filterCenter) - filterCenter; %Distancias da sinc até a amostra mais próxima
-            gapLeft = filterCenter - floor(filterCenter);
-            while (counter < halfFilterCoeffs) %Calcula 2 termos por vez, do centro da sinc até as extremidades
-                leftConvolutionIndex = floor(filterCenter) - counter;
-                rightConvolutionIndex = ceil(filterCenter) + counter;
-                if ( leftConvolutionIndex > 0) %Calcula lado esquerdo da sinc
-                    sample = sample + inputSignal(leftConvolutionIndex)*sinc(gapLeft);
-                end
-                if (rightConvolutionIndex < signalSize) %Calcula lado direito da sinc
-                    sample = sample + inputSignal(rightConvolutionIndex)*sinc(gapRight);
-                end
-                counter = counter + 1; % percorre os pontos de encontro entre a sinc e o sinal.
-                gapLeft = gapLeft + 1;
-                gapRight = gapRight + 1;
+            while (originalIndex <= filterCoeffs)
+                sample = sample + inputSignal(originalIndex)*sinc(newIndex - (originalIndex-1));
+                originalIndex = originalIndex + 1;
             end
-            outputSignal(currentSampleIndex) = sample;
-        end
-        currentSampleIndex = currentSampleIndex + 1;
-        filterCenter = filterCenter + newPeriod;
+        newIndex = newIndex + newPeriod;
+        outputSignal(currentSampleIndex) = sample;
     end
-    
-    y = outputSignal;
+
 
 end
