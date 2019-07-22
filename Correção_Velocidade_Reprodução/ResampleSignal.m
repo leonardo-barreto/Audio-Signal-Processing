3fc34db961f00c1df9dcfcfeb54e6ca40043c49d
Leonardo Barreto <52412843+leonardo-barreto@users.noreply.github.com>
2019-07-22 17:10:56 -0300

    Updated ResampleSignal.m
    Completely redesigned.

--- Correção_Velocidade_Reprodução/ResampleSignal.m
+++ Correção_Velocidade_Reprodução/ResampleSignal.m
@@ -4,28 +4,61 @@
 
 %Saidas: Sinal reamostrado: (outputSignal)
 
-function outputSignal = ResampleSignal(resampleFactor,filterCoeffs,inputSignal)
+function y = ResampleSignal(resampleFactor,filterCoeffs,inputSignal)
     
     %Definitions
-    signalSize = length(inputSignal);
-    newIndex = 0;
-    currentSampleIndex = 1;
+    inputSignalSize = length(inputSignal);
+    newSamplePosition = 1;
+    resampledIndex = 1;
     newPeriod = (1/resampleFactor);
 
     %Outputs
-    outputSignal=zeros(1,signalSize);
+    outputSignal = zeros(1,inputSignalSize); % Para prevenir erros de undefined variable
    
     %Code
-    while newIndex < signalSize
-            originalIndex = 1;
+    while newSamplePosition <= inputSignalSize % percorre o sinal de entrada no novo período de amostragem
+            convolutionIndex = 1; % índice interno que percorre o sinal de entrada para a multiplicação pela sinc
             sample = 0;
-            while (originalIndex <= filterCoeffs)
-                sample = sample + inputSignal(originalIndex)*sinc(newIndex - (originalIndex-1));
-                originalIndex = originalIndex + 1;
+            if CheckInteger(newSamplePosition) % se a amostra na nova taxa coincide com a amostra na taxa antiga
+                outputSignal(resampledIndex) = inputSignal(newSamplePosition);
+            else
+                if CheckInteger(2*newSamplePosition) %se a casa decimal for .5 (floor e ceil se confundem)
+                    leftGap = 0.5; % leftGap é a distância da nova amostra para a primeira amostra a sua esquerda.
+                    rightGap = 0.5; % rightGap a mesma coisa, mas para a primeira amostra a sua direita.
+                    while convolutionIndex <= round(filterCoeffs/2)
+                        %disp '0.5 found!'
+                        %convolutionIndex
+                        %pause(1);
+                        %clc;
+                        convolutionIndex_left = (newSamplePosition - leftGap) - (convolutionIndex-1);
+                        convolutionIndex_right = (newSamplePosition + rightGap) + (convolutionIndex-1);
+                        if convolutionIndex_left > 0
+                            sample = sample + inputSignal(convolutionIndex_left)*sinc(-leftGap - (convolutionIndex-1));
+                        end
+                        if  convolutionIndex_right < inputSignalSize
+                            sample = sample + inputSignal(convolutionIndex_right)*sinc(rightGap + (convolutionIndex-1));
+                        end
+                        convolutionIndex = convolutionIndex + 1;
+                    end
+                else % qualquer casa decimal que não seja 0.5
+                    leftGap = newSamplePosition - floor(newSamplePosition);
+                    rightGap = ceil(newSamplePosition) - newSamplePosition;
+                    while convolutionIndex <= round(filterCoeffs/2)
+                        convolutionIndex_left = (newSamplePosition - leftGap) - (convolutionIndex-1);
+                        convolutionIndex_right = (newSamplePosition + rightGap) + (convolutionIndex-1);
+                        if convolutionIndex_left > 0
+                            sample = sample + inputSignal(convolutionIndex_left)*sinc(-leftGap - (convolutionIndex-1));
+                        end
+                        if  convolutionIndex_right < inputSignalSize
+                            sample = sample + inputSignal(convolutionIndex_right)*sinc(rightGap + (convolutionIndex-1));
+                        end
+                        convolutionIndex = convolutionIndex + 1;
+                    end
+                end
+                outputSignal(resampledIndex) = sample;
             end
-        newIndex = newIndex + newPeriod;
-        outputSignal(currentSampleIndex) = sample;
+            newSamplePosition = newSamplePosition + newPeriod;
+            resampledIndex = resampledIndex + 1;
     end
-
-
+    y = outputSignal;
 end
