e3df58823d216ed20271765a673398436996aa44
Leonardo Barreto <52412843+leonardo-barreto@users.noreply.github.com>
2020-05-19 22:04:04 -0300

    nothing special

--- Correção_Velocidade_Reprodução/Measurements/GenSamplingCurve.m
+++ Correção_Velocidade_Reprodução/Measurements/GenSamplingCurve.m
@@ -1,36 +1,28 @@
-function [timePositions,signalLength] = GenSamplingCurve (totalBlocks,blockSize)
+function [timePositions,samplingRateArray,signalLength] = GenSamplingCurve (baseSamplingRate,totalBlocks,blockSize)
 
 %This function will try to generate a custom sampling curve in order to generate arbitrarily sampled signals.
 
-baseSamplingRate = 48000; %block calculations have to interpret the signal as uniformly sampled. 
+%baseSamplingRate = 48000; %block calculations have to interpret the signal as uniformly sampled. 
                           %Therefore, window length in seconds is given by this rate.
-
-%windowLength = windowSize/baseSamplingRate;
-%windowCenter = windowSize/2;
-%blockSize = windowSize;
 
 signalLength = 0; %time length
 timePositions = [];
 blocks = [];
 
-%samples = 0;
-%while (samples < blockSize/2)
-%    currentPeriod = 1/baseSamplingRate;
-%    signalLength = signalLength + currentPeriod;
-%    timePositions(end+1) = signalLength;
-%    samples = samples + 1;
-%end
-
 % GENERATE THE ARRAY OF SAMPLING RATES!
 
 samplingRateArray = baseSamplingRate*ones(totalBlocks,1);
-randomArray = -baseSamplingRate/12 + (2*baseSamplingRate/12)*rand(totalBlocks,1);
 
-samplingRateArray = samplingRateArray + randomArray;
-    
+% Non-Uniform Function
+
+%functionArray = 4000*sin(pi*(0:1/baseSamplingRate:(totalBlocks-1/baseSamplingRate)));
+functionArray = 4000*sin(2*pi*(0:1/totalBlocks:1-1/totalBlocks));
+
+samplingRateArray = samplingRateArray + functionArray;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for currentRate = samplingRateArray
+for currentRate = transpose(samplingRateArray)
     samples = 0;
     while (samples < blockSize)
         currentPeriod = 1/currentRate;
